165c4430af150f70bdb9871605e91e486a01194e
Wenjie <wenjietj@gmail.com>
2021-05-15 08:57:47 +0800

    remove namespaces

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from .views import *
 from rest_framework_simplejwt.views import (TokenObtainPairView)
 
-app_name = "accounts"  # namespace
-
 urlpatterns = [
     path('register/', views_register, name='register'),
     path('login/', views_login, name='login'),
--- articles/urls.py
+++ articles/urls.py
@@ -1,8 +1,5 @@
 from django.urls import path
 from .views import *
 
-app_name = "articles"  # namespace
-
 urlpatterns = [
-
 ]
