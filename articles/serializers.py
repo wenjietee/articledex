71211c06e2ace7b15a65769bb04e5a46570fa713
Wenjie <wenjietj@gmail.com>
2021-05-16 14:56:09 +0800

    update tags serializer

--- articles/serializers.py
+++ articles/serializers.py
@@ -5,12 +5,24 @@ from .models import *
 
 
 class TagSerializer(serializers.ModelSerializer):
+    articles = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='title'
+    )
+
     class Meta:
         model = Tag
         fields = '__all__'
 
 
 class ArticleSerializer(serializers.ModelSerializer):
+    tags = serializers.SlugRelatedField(
+        many=True,
+        queryset=Tag.objects.all(),
+        slug_field='text'
+    )
+
     class Meta:
         model = Article
         fields = '__all__'
