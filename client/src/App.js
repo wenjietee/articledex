efad9a08b9387a5aaf476ac82e18dca0056d4ac8
Wenjie <wenjietj@gmail.com>
2021-05-19 22:39:47 +0800

    added page routes to app

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ import PublicRoute from './components/PublicRoute';
 import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
-	const [user, setUser] = useState({ isAuth: false });
+	const [isAuth, setUser] = useState(false);
 
 	// handle login
 	async function login(e, username, password) {
@@ -31,7 +31,7 @@ function App() {
 			localStorage.setItem('refresh', data.refresh);
 
 			// login user
-			setUser({ isAuth: true });
+			setUser(true);
 		} catch (error) {
 			console.log(error.response);
 		}
@@ -45,21 +45,66 @@ function App() {
 		localStorage.removeItem('refresh');
 
 		// logout user
-		setUser({ isAuth: false });
+		setUser(false);
 	}
 	return (
 		<div className='App'>
 			<Router>
 				<Switch>
-					<Route
+					<PublicRoute
 						exact
 						path='/'
-						login={login}
-						render={(props) => (
-							<Landing {...props} user={user} login={login} />
-						)}
+						isAuth={isAuth}
+						component={Landing}
 					/>
-					<ProtectedRoute exact path='/home' component={Home} />
+					<PublicRoute
+						exact
+						path='/login'
+						isAuth={isAuth}
+						component={Login}
+					/>
+					<PublicRoute
+						exact
+						path='/register'
+						isAuth={isAuth}
+						component={Register}
+					/>
+					<ProtectedRoute
+						exact
+						path='/home'
+						isAuth={isAuth}
+						component={Home}
+					/>
+					<ProtectedRoute
+						exact
+						path='/article/create'
+						isAuth={isAuth}
+						component={ArticleForm}
+					/>
+					<ProtectedRoute
+						exact
+						path='/article/:id'
+						isAuth={isAuth}
+						component={Article}
+					/>
+					<ProtectedRoute
+						exact
+						path='/article/:id/edit'
+						isAuth={isAuth}
+						component={ArticleForm}
+					/>
+					<ProtectedRoute
+						exact
+						path='/profile'
+						isAuth={isAuth}
+						component={Profile}
+					/>
+					<ProtectedRoute
+						exact
+						path='/profile/edit'
+						isAuth={isAuth}
+						component={ProfileEdit}
+					/>
 					<Route exact path='/404' component={NotFound} />
 					<Redirect to='/404' />
 				</Switch>
