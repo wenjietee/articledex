27a9329fe7bb9e8af4b36a730e9dfd4b7b985db7
Wenjie <wenjietj@gmail.com>
2021-05-23 19:14:43 +0800

    add image to article card

--- client/src/components/ArticleCard.jsx
+++ client/src/components/ArticleCard.jsx
@@ -3,9 +3,11 @@ import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
+import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import { CardActionArea } from '@material-ui/core';
 
 const useStyles = makeStyles({
 	root: {
@@ -17,6 +19,9 @@ const useStyles = makeStyles({
 	pos: {
 		marginBottom: 12,
 	},
+	media: {
+		height: 140,
+	},
 });
 
 const ArticleCard = (props) => {
@@ -24,6 +29,13 @@ const ArticleCard = (props) => {
 
 	return (
 		<Card raised className={classes.root}>
+			<CardActionArea>
+				<CardMedia
+					className={classes.media}
+					image={props.article.image}
+					title='article'
+				/>
+			</CardActionArea>
 			<CardContent>
 				<Typography variant='h5' component='h2'>
 					{props.article.title}
