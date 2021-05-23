3cbeb1c346b2a8b140210447a41eb6a1f49e59ff
Wenjie <wenjietj@gmail.com>
2021-05-23 18:13:32 +0800

    unread working on client

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,9 +2,13 @@ import React, { useState, useEffect } from 'react';
 import Axios from '../utils/Axios';
 
 const Home = (props) => {
+	// states
 	const [articles, setArticles] = useState();
+	const [unreads, setUnreads] = useState();
 
+	// on load get data
 	useEffect(() => {
+		// get articles
 		try {
 			Axios.get(`${process.env.REACT_APP_URL}api/articles/`).then(
 				(response) => {
@@ -15,11 +19,23 @@ const Home = (props) => {
 		} catch (error) {
 			console.log(error);
 		}
+		// get user unreads
+		try {
+			Axios.get(`${process.env.REACT_APP_URL}api/unreads/`).then(
+				(response) => {
+					// set state with article
+					setUnreads(response.data);
+				}
+			);
+		} catch (error) {
+			console.log(error);
+		}
 	}, []);
 
 	return (
 		<div>
 			<pre>{JSON.stringify(articles, null, 2)}</pre>
+			<pre>{JSON.stringify(unreads, null, 2)}</pre>
 		</div>
 	);
 };
