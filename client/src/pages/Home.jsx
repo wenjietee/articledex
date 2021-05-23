5b6dcefa4595b72ff1eda0025baec523e5d824a6
Wenjie <wenjietj@gmail.com>
2021-05-23 17:50:25 +0800

    able to get all articless to client

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Axios from '../utils/Axios';
 
 const Home = (props) => {
+	const [articles, setArticles] = useState();
+
+	useEffect(() => {
+		try {
+			Axios.get(`${process.env.REACT_APP_URL}api/articles/`).then(
+				(response) => {
+					// set state with article
+					setArticles(response.data);
+				}
+			);
+		} catch (error) {
+			console.log(error);
+		}
+	}, []);
+
 	return (
 		<div>
-			<h1>Home</h1>
+			<pre>{JSON.stringify(articles, null, 2)}</pre>
 		</div>
 	);
 };
