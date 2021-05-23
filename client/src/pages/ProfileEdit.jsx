627e59203b07f4f7219de87adff60817a4f562a7
Wenjie <wenjietj@gmail.com>
2021-05-23 16:26:09 +0800

    profile edit redirect working

--- client/src/pages/ProfileEdit.jsx
+++ client/src/pages/ProfileEdit.jsx
@@ -1,11 +1,13 @@
 import Axios from '../utils/Axios';
 import React, { useState, useEffect } from 'react';
+import { Redirect } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Box from '@material-ui/core/Box';
 import ProfileEditForm from '../components/ProfileEditForm';
 
 const ProfileEdit = () => {
 	const [profile, setProfile] = useState();
+	const [isSubmitted, setSubmitted] = useState(false);
 	useEffect(() => {
 		// fetched article
 		try {
@@ -29,14 +31,16 @@ const ProfileEdit = () => {
 				description: inputs.description,
 				image: inputs.image,
 			});
-			alert(`Profile edited`);
+			setSubmitted(true);
 		} catch (error) {
 			alert(
 				`Error ${error.response.status}: ${error.response.data.detail}`
 			);
 		}
 	};
-
+	if (isSubmitted) {
+		return <Redirect to='/profile' />;
+	}
 	return (
 		<React.Fragment>
 			<CssBaseline />
