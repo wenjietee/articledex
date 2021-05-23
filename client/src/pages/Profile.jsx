10404fcf708f7a159aef50980d4e74029b05ca76
Wenjie <wenjietj@gmail.com>
2021-05-23 22:26:04 +0800

    able to fetch profile to client

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Axios from '../utils/Axios';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
-const Profile = () => {
+const Profile = (props) => {
+	// states
+	const [profile, setProfile] = useState();
+
+	// get profile
+	useEffect(() => {
+		// get articles
+		try {
+			Axios.get(`${process.env.REACT_APP_URL}api/profile/`).then(
+				(response) => {
+					// set state with article
+					setProfile(response.data);
+				}
+			);
+		} catch (error) {
+			console.log(error);
+		}
+	}, []);
+
 	return (
-		<div>
+		<React.Fragment>
 			<h1>Profile page</h1>
+
 			<Button
 				variant='contained'
+				color='primary'
 				disableElevation={true}
 				component={Link}
 				to='/profile/edit'
 			>
 				EDIT PROFILE
 			</Button>
-		</div>
+			<pre>{JSON.stringify(profile, null, 2)}</pre>
+		</React.Fragment>
 	);
 };
 
