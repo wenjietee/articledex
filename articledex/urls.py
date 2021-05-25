a7f69f4635850cc72b8e6a9b56645500bdb89f00
Wenjie <wenjietj@gmail.com>
2021-05-25 20:38:59 +0800

    fix  interceptor bug

--- articledex/urls.py
+++ articledex/urls.py
@@ -17,14 +17,14 @@ from django.contrib import admin
 from django.conf import settings
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('accounts.urls')),
     path('api/articles/', include('articles.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/', TokenRefreshView.as_view(), name='token_refresh_view'),
     re_path('(^(?!(api|admin)).*$)',
             TemplateView.as_view(template_name='index.html'))
 ]
