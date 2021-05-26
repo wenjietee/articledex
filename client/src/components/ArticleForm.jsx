1f457f86fc1c185e51ebdcebb4882cf25f83aadd
Wenjie <wenjietj@gmail.com>
2021-05-26 11:07:52 +0800

    add grid layout ot article form

--- client/src/components/ArticleForm.jsx
+++ client/src/components/ArticleForm.jsx
@@ -6,6 +6,8 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 
 // form styles
 const useStyles = makeStyles((theme) => ({
@@ -63,15 +65,19 @@ const ArticleForm = (props) => {
 			>
 				{({ values, isSubmitting }) => (
 					<Form>
-						<div>
-							<Box pt={2}>
+						<Grid
+							container
+							spacing={3}
+							className={classes.textField}
+						>
+							<Grid item xs={8}>
 								<Field
 									name='url'
 									placeholder='www.myarticle.com/'
 									label='URL*'
 									type='input'
+									fullWidth
 									variant='outlined'
-									className={classes.textField}
 									as={TextField}
 									value={values.url}
 								/>
@@ -79,16 +85,14 @@ const ArticleForm = (props) => {
 								<Typography color='error'>
 									<ErrorMessage name='url' />
 								</Typography>
-							</Box>
-						</div>
-						<div>
-							<Box pt={2}>
+							</Grid>
+							<Grid item xs={4}>
 								<Field
 									name='article_type'
 									label='Article Type'
 									type='input'
 									variant='outlined'
-									className={classes.textField}
+									fullWidth
 									as={TextField}
 									value={values.article_type}
 								/>
@@ -96,27 +100,23 @@ const ArticleForm = (props) => {
 								<Typography color='error'>
 									<ErrorMessage name='url' />
 								</Typography>
-							</Box>
-						</div>
-						<div>
-							<Box pt={2}>
+							</Grid>
+							<Grid item xs={12}>
 								<Field
 									name='title'
 									placeholder='article title'
 									label='Title*'
 									type='text'
 									variant='outlined'
-									className={classes.textField}
+									fullWidth
 									as={TextField}
 									value={values.title}
 								/>
 								<Typography color='error'>
 									<ErrorMessage name='title' />
 								</Typography>
-							</Box>
-						</div>
-						<div>
-							<Box pt={2}>
+							</Grid>
+							<Grid item xs={12}>
 								<Field
 									name='description'
 									placeholder='Description'
@@ -126,26 +126,25 @@ const ArticleForm = (props) => {
 									multiline
 									rows={10}
 									rowsMax={10}
-									className={classes.textField}
+									fullWidth
 									as={TextField}
 									value={values.description}
 								/>
-							</Box>
-						</div>
-						<div>
-							<Box pt={2}>
+							</Grid>
+							<Grid item xs={12}>
 								<Field
 									name='tags'
 									placeholder='Seperate tags with spacing. Eg. Tag1 Tag2'
 									label='Tags'
 									type='text'
 									variant='outlined'
-									className={classes.textField}
+									fullWidth
 									as={TextField}
 									value={values.tags}
 								/>
-							</Box>
-						</div>
+							</Grid>
+						</Grid>
+
 						<Box pt={2} align='center'>
 							<Button
 								variant='contained'
