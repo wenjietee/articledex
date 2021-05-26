e120aa1e1c85940a198f5f50531c5eaadc07abe5
Wenjie <wenjietj@gmail.com>
2021-05-26 12:20:06 +0800

    able to toggle buttons

--- client/src/components/UserActionPopover.jsx
+++ client/src/components/UserActionPopover.jsx
@@ -2,13 +2,21 @@ import React, { useState } from 'react';
 import Popover from '@material-ui/core/Popover';
 import IconButton from '@material-ui/core/IconButton';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
-import SaveAltIcon from '@material-ui/icons/SaveAlt';
+import GetAppIcon from '@material-ui/icons/GetApp';
+import PublishIcon from '@material-ui/icons/Publish';
 import VisibilityIcon from '@material-ui/icons/Visibility';
+import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';
 import SubjectIcon from '@material-ui/icons/Subject';
+import PlaylistAddCheckIcon from '@material-ui/icons/PlaylistAddCheck';
 
 const UserActionPopover = (props) => {
+	// states
 	const [anchorEl, setAnchorEl] = useState(null);
+	const [isLocal, setLocal] = useState();
+	const [isUnread, setUnread] = useState();
+	const [isPrivate, setPrivate] = useState();
 
+	// toggle popover
 	const handleClick = (event) => {
 		setAnchorEl(event.currentTarget);
 	};
@@ -20,6 +28,20 @@ const UserActionPopover = (props) => {
 	const open = Boolean(anchorEl);
 	const id = open ? 'simple-popover' : undefined;
 
+	// toggle unread
+	const toggleUnread = () => {
+		setUnread(!isUnread);
+	};
+
+	// toggle private
+	const togglePrivate = () => {
+		setPrivate(!isPrivate);
+	};
+	// toggle local
+
+	const toggleLocal = () => {
+		setLocal(!isLocal);
+	};
 	return (
 		<React.Fragment>
 			<IconButton
@@ -43,14 +65,38 @@ const UserActionPopover = (props) => {
 					horizontal: 'center',
 				}}
 			>
-				<IconButton aria-describedby={id} color='primary'>
-					<SubjectIcon fontSize='large' />
+				<IconButton
+					aria-describedby={id}
+					color='primary'
+					onClick={toggleUnread}
+				>
+					{isUnread ? (
+						<PlaylistAddCheckIcon fontSize='default' />
+					) : (
+						<SubjectIcon fontSize='default' />
+					)}
 				</IconButton>
-				<IconButton aria-describedby={id} color='primary'>
-					<VisibilityIcon fontSize='large' />
+				<IconButton
+					aria-describedby={id}
+					color='primary'
+					onClick={togglePrivate}
+				>
+					{isPrivate ? (
+						<VisibilityOffIcon fontSize='default' />
+					) : (
+						<VisibilityIcon fontSize='default' />
+					)}
 				</IconButton>
-				<IconButton aria-describedby={id} color='primary'>
-					<SaveAltIcon fontSize='large' />
+				<IconButton
+					aria-describedby={id}
+					color='primary'
+					onClick={toggleLocal}
+				>
+					{isLocal ? (
+						<PublishIcon fontSize='default' />
+					) : (
+						<GetAppIcon fontSize='default' />
+					)}
 				</IconButton>
 			</Popover>
 		</React.Fragment>
