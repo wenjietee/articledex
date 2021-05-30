e4021cebc920a4f2293e85c5ce5ca7a27d79aad1
Wenjie <wenjietj@gmail.com>
2021-05-30 10:13:18 +0800

    update url patterns

--- articledex/urls.py
+++ articledex/urls.py
@@ -18,7 +18,6 @@ from django.conf import settings
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from rest_framework_simplejwt.views import TokenRefreshView
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
